99a6196eb9c27fcd0ab85219090689b827bb1093
Rasmus <rasm3600@student.eadania.dk>
2020-05-25 09:39:35 +0200

    new serverjs

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angquestcharlie'));
+app.use(express.static(__dirname + '/dist/ang-quest'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/angquestcharlie/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ang-quest/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
